d815cf164c74d80bce84701701f6be2261c95d72
abishekn1 <abishekraj0405@gmail.com>
2023-05-23 19:08:58 +0530

    application blank push

---
+++ src/test/java/com/xworkz/xworkzapi/XworkzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xworkz.xworkzapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XworkzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
