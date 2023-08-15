702d7dc9d0d2def0b143fad270495baaeae61638
X-workzDev01 <x-workzdev01>
2023-08-15 15:01:43 +0530

    new code

--- src/main/java/com/xworkz/dream/util/UtilDev.java
+++ src/main/java/com/xworkz/dream/util/UtilDev.java
@@ -13,13 +13,17 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Component
 @Profile("dev")
@@ -34,6 +38,7 @@ public class UtilDev implements DreamUtil {
 	@Value("${mail.password}")
 	private String password;
 
+	
 	private static final Logger logger = LoggerFactory.getLogger(UtilLocal.class);
 
 	public int generateOTP() {
@@ -95,58 +100,10 @@ public class UtilDev implements DreamUtil {
 		return Base64.getEncoder().encodeToString(tokenBytes);
 	}
 
-	public boolean sendCourseContent(String email,String name) {
-		String subject = "Java Full Stack Course Content";
-		String body = "Dear Candidate,\n\n"
-				+ "Please find the attached Java Full Stack Course Content for your reference.\n\n"
-				+ "The attached file contains the overview of the course. Apart from the mentioned course, "
-				+ "you will be learning a total of 15 Technologies and Placement activities, technical aptitude, "
-				+ "Resume Building, Mocks, Presentation, etc.\n\n"
-				+ "For any queries, feel free to reach me at 9886971483/9886971480.\n\n"
-				+ "Follow us on Facebook and Instagram for more updates regarding new batches and placement details:\n"
-				+ "Facebook: https://www.facebook.com/xworkzdevelopmentcenter/\n"
-				+ "Instagram: https://www.instagram.com/xworkzodc1?r=nametag\n"
-				+ "Google Reviews Link: https://g.page/xworkzodc/review?av\n\n" + "Thanks and Regards,\n"
-				+ "HR Team\n" + "M.No: 9845958884\n\n"
-				+ "Find us on Google Maps: https://goo.gl/maps/LSgLsZ6oqRu\n\n" + "https://www.x-workz.in\n"
-				+ "080-48669257/9845958884\n" + "RAJAJINAGAR||BTM";
-		// Email properties
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.office365.com");
-		props.put("mail.smtp.port", smtpPort);
-
-		// Create session with authentication
-		Session session = Session.getInstance(props, new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(userName, password);
-			}
-		});
-		try {
-			// Create email message
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-			message.setSubject(subject);
-			message.setText(body);
-			
-			// Send email
-			Transport.send(message);
-			logger.info("Email sent to {}: Subject: {}", email, subject);
-			return true; // Email sent successfully
-		} catch (MessagingException e) {
-			System.out.println("");
-			logger.error("Failed to send email ", e);
-			e.printStackTrace();
-			return false; // Failed to send email
-		}
-		
-	}
+	@Override
+	public boolean sendCourseContent(String email, String name) {
 	
-	public boolean sendMail(String name,String email) {
-		String subject = "Java Full Stack Course Content";
-		
 		return false;
 	}
+
 }
