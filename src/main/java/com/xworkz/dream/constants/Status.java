36a146d754c530710580aeccca884c0041c61253
X-workzDev01 <hareeshahr.xworkz@gmail.com>
2023-12-01 16:26:54 +0530

    fees issue fixed

--- src/main/java/com/xworkz/dream/constants/Status.java
+++ src/main/java/com/xworkz/dream/constants/Status.java
@@ -2,8 +2,9 @@ package com.xworkz.dream.constants;
 
 public enum Status {
 
-	NO,NA, New, Joining, Interested, RNR, Not_interested, Busy, Incomingcall_not_available,
+	ENQUIRY, NO, NA, New, Joining, Interested, RNR, Not_interested, Busy, Incomingcall_not_available,
 
-	Wrong_number, Not_reachable, Not_joining, Joined_other_institute, Higher_studies, Let_us_know, Need_online, Joined ,FollowUpComplete,FollowUpInComplete;
+	Wrong_number, Not_reachable, Not_joining, Joined_other_institute, Higher_studies, Let_us_know, Need_online, Joined,
+	FollowUpComplete, FollowUpInComplete;
 
 }
