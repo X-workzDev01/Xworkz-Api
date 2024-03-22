7fed5a42d7f12fdd8d7f021b7339d8f6c969bcf8
X-workzDev01 <x-workzdev01>
2024-03-22 17:39:17 +0530

    removed 2m schedular

--- src/main/java/com/xworkz/dream/scheduler/BirthdaySchedulerImpl.java
+++ src/main/java/com/xworkz/dream/scheduler/BirthdaySchedulerImpl.java
@@ -14,8 +14,7 @@ public class BirthdaySchedulerImpl {
 	private BirthadayService birthadayService;
 	Logger logger = LoggerFactory.getLogger(BirthdaySchedulerImpl.class);
 
-	//@Scheduled(cron = "0 0 0 * * *")
-	@Scheduled(cron = "0 */2 0 * * *")
+	@Scheduled(cron = "0 0 0 * * *")
 	public void sendBirthdayEmailsScheduled() {
 		logger.info("Running sendBirthday Scheduler");
 		birthadayService.sendBirthdayEmails();
